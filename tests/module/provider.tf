54256d0aff98b9d9315b1d8a55a6c11c3dc0f4f9
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-16 13:49:11 +0100

    update test workflow

--- tests/module/provider.tf
+++ tests/module/provider.tf
@@ -6,15 +6,6 @@ terraform {
     }
   }
   required_version = "~> 0.14"
-
-  backend "azurerm" {
-    storage_account_name = "hybridaccessterraform"
-    container_name       = "tfstate"
-    key                  = "cassandra.tfstate"
-    subscription_id      = "02b44a0d-f98e-407e-b0c1-7d394305857e"
-    tenant_id            = "c555cdfa-e1fa-4957-b451-1ff2ce91d0a7"
-    resource_group_name  = "terraform-bootstrap"
-  }
 }
 
 provider "azurerm" {
